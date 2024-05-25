99edb09346de17bef601d090f878b820f3efc788
Dan Toms <157254317+pynmash@users.noreply.github.com>
2024-05-25 09:20:10 +0100

    Implement MariaDB

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sqlite3
+import mysql.connector
 import sys
 
 from PyQt6.QtCore import Qt
@@ -9,11 +9,16 @@ from PyQt6.QtWidgets import QApplication, QDialog, QGridLayout, QLabel, \
 
 
 class DatabaseConnection:
-    def __init__(self, database_file="database.db"):
-        self.database_file = database_file
+    def __init__(self, host="localhost", user="dan", password="password", database="school"):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
 
     def connect(self):
-        connection = sqlite3.connect(self.database_file)
+        connection = mysql.connector.connect(host=self.host, user=self.user,
+                                             password=self.password,
+                                             database=self.database)
         return connection
 
 
@@ -81,7 +86,9 @@ class MainWindow(QMainWindow):
 
     def load_data(self):
         connection = DatabaseConnection().connect()
-        result = connection.execute("SELECT * FROM students")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM students")
+        result = cursor.fetchall()
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(result):
             self.table.insertRow(row_number)
@@ -148,7 +155,7 @@ class EditDialog(QDialog):
 
         # Add combo box of courses
         self.course_name = QComboBox()
-        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        courses = ["Biology", "Maths", "Astronomy", "Physics"]
         self.course_name.addItems(courses)
         self.course_name.setCurrentText(course_name)
         layout.addWidget(self.course_name)
@@ -171,7 +178,7 @@ class EditDialog(QDialog):
         connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         cursor.execute(
-            "UPDATE students SET name = ?, course = ?, mobile = ? WHERE id = ?", 
+            "UPDATE students SET name = %s, course = %s, mobile = %s WHERE id = %s", 
                        (self.student_name.text(), 
                         self.course_name.itemText(
                             self.course_name.currentIndex()), 
@@ -212,7 +219,7 @@ class DeleteDialog(QDialog):
 
         connection = DatabaseConnection().connect()
         cursor = connection.cursor()
-        cursor.execute("DELETE from students WHERE id = ?", (student_id, ))
+        cursor.execute("DELETE from students WHERE id = %s", (student_id, ))
         connection.commit()
         cursor.close()
         connection.close()
@@ -266,7 +273,7 @@ class InsertDialog(QDialog):
         connection = DatabaseConnection().connect()
         cursor = connection.cursor()
         cursor.execute(
-            "INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)", 
+            "INSERT INTO students (name, course, mobile) VALUES (%s, %s, %s)", 
             (name, course, mobile))
         connection.commit()
         cursor.close()
@@ -298,8 +305,9 @@ class SearchDialog(QDialog):
         name = self.search_term.text()
         connection = DatabaseConnection().connect()
         cursor = connection.cursor()
-        result = cursor.execute(
-            "SELECT * FROM students WHERE name = ?", (name,))
+        cursor.execute(
+            "SELECT * FROM students WHERE name = %s", (name,))
+        result = cursor.fetchall()
         rows = list(result)
         print(rows)
         items = main_window.table.findItems(name, Qt.MatchFlag.MatchFixedString)
